c9074b3517799ec1c4ac624f393b47790ee1befe
Michał Sokół <msokol@localhost.localdomain>
2016-04-26 00:04:12 +0200

    Second Commit
    Still not much

--- src/Server/Connection.java
+++ src/Server/Connection.java
@@ -8,22 +8,22 @@ import java.net.Socket;
 public class Connection extends Thread {
 
 	private Socket socket;
-	private int connectionNumber;
+	private int playerID;
 
-	public Connection(Socket socket, int connectionNumber) {
+	public Connection(Socket socket, int playerID) {
 		this.socket = socket;
-		this.connectionNumber = connectionNumber;
+		this.playerID = playerID;
 	}
 
 	public void run() {
 		try {
 			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-			out.println("Gracz nr : " + connectionNumber + ".");
+			out.println("Gracz nr : " + playerID + ".");
 			String input = in.readLine();
 			while (!input.equals("quit")) {
 				input = in.readLine();
-				System.out.println("Wiadomość od gracza nr " + connectionNumber + " :\n" + input);
+				System.out.println("Wiadomość od gracza nr " + playerID + " :\n" + input);
 				out.println("Received message");
 			}
 			socket.close();
